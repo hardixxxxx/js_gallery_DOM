c7a936c7a242fb501b3b127a5796aa383f6f124e
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-13 19:38:04 +0200

    make build

---
+++ index.dc816838.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery__list").onclick=function(e){e.preventDefault();var t=document.querySelector("#largeImg"),l=e.target.closest(".list-item__link").href;l&&(t.src=l)};
+//# sourceMappingURL=index.dc816838.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const galleryList = document.querySelector(".gallery__list");
+galleryList.onclick = (ev)=>{
+    ev.preventDefault();
+    const largeImg = document.querySelector("#largeImg");
+    const link = ev.target.closest(".list-item__link").href;
+    if (!link) return;
+    largeImg.src = link;
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
